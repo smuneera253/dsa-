9fd6524db1a45e681bb5a4881b27709ff3c3a502
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:45:03 +0530

    done
    done

--- leftevenrightoddD1.java
+++ leftevenrightoddD1.java
@@ -1,23 +1,29 @@
 class main
 {
 	public static void main(String[]args)
-	{
+	{	
+		//creat an arr
 		int []arr={2,3,4,6,5,1,8};
-		int start=0;
+		//we have to fine to point
+		int start=0;	
 		int end=arr.length -1;
 		
+		//we run the  while loop
 		while(start<end)
 		{
+			//we check the  condition even or not
 			while(arr[start]%2==0)
 			{
 				start++;
 			}
+			//we check the  condition odd or not
 			while(arr[end]%2!=0)
 			{
 				end--;
 			}
-			if(start<end)
+			if(start<end)//again we have to check the condition
 			{
+				//after check the condition we have to swape
 				int temp=arr[start];
 				arr[start]=arr[end];
 				arr[end]=temp;
@@ -27,6 +33,8 @@ class main
 			}
 		
 		}
+		
+		//after swap the condion  we have to print in right way
 		for(int i=0;i<arr.length;i++)
 		{
 			System.out.print(arr[i]+",");
