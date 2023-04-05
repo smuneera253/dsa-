685e683d31a5ff53126f4a83d8461da8acb16140
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:39:22 +0530

    done
    done

--- primenotd1.java
+++ primenotd1.java
@@ -2,13 +2,13 @@ public class main
 {
   public static void main(String[]args)
   {
-  
+  //creat one number
   int num=8;
   int count=0;
-  	
+  	//we run the loop
   	for(int i=1;i<=num;i++)
   	{
-  		if(num%i==0)
+  		if(num%i==0)/ /check the if number is prime or not
   		{
   			count++;
   		}
