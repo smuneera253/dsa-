af2f9b75ac440d464186eeaee243936bf016a2ac
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:54:03 +0530

    Update reversd1.java

--- reversd1.java
+++ reversd1.java
@@ -2,13 +2,16 @@
 class HelloWorld {
     //creat an string
     public static String str="muneera";
+    
     //convert str into arr;
+    
     public static char[] arr = str.toCharArray();
+    
     public static void main(String[] args)
     {
         System.out.println(arr);
-        ReverseSubString(0 , arr.length-1 , arr);//reverse the the string
-        System.out.println(arr);// print the revers string
+        ReverseSubString(0 , arr.length-1 , arr); //reverse the the string
+        System.out.println(arr);  // print the revers string
     }
     public static void ReverseSubString(int start , int end ,char[] arr)
     {
