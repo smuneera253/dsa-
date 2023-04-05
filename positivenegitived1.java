5b6e9a94e8bc0bb8ccdc759caa155ccd90645b44
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:33:27 +0530

    done
    done

---
+++ positivenegitived1.java
@@ -0,0 +1,18 @@
+class main
+{
+	public static void main(String[]args)
+	{
+		int num=-20;
+		
+		if(num>0)
+		{
+			System.out.println("Positive");
+		}else
+		{
+			System.out.println("Negitive");
+		
+		}
+	
+	}
+
+}
