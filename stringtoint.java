0977451f86b4b1a4d197bd3af7c1bc640d96012c
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:53:20 +0530

    done
    done

--- stringtoint.java
+++ stringtoint.java
@@ -2,9 +2,9 @@ class main
 {
 	public static void main(String[]args)}
 	{
-		int [] arr={1,2,36,7,8};
+		int [] arr = {1,2,36,7,8};
 		
-		for(int i=0;i<arr.length;i++)
+		for(int i = 0; i < arr.length; i++)
 		{
 			System.out.println(arr[i]+" ");
 		}
