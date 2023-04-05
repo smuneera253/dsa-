cac9cc2d6c0331911a851c0cbd5aa285682b5c3d
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:48:21 +0530

    done
    done

--- arrelement.java
+++ arrelement.java
@@ -2,9 +2,10 @@ class elementsarr
 {
 	public static void main(String[]args)
 	{
-	       int[] arr={9,8,3,4,3,6};
+	
+	       int[] arr = {9,8,3,4,3,6};
 		
-		for(int i=0;i<arr.length;i++)
+		for(int i = 0; i < arr.length;i++)
 		{
 			System.out.print(arr[i]);			
 		}
