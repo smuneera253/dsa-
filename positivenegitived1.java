b2903d6a7f8420f11043f4707be7dfdee66aaf2f
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:40:59 +0530

    done
    done

--- positivenegitived1.java
+++ positivenegitived1.java
@@ -1,10 +1,10 @@
 class main
 {
 	public static void main(String[]args)
-	{
+	{	//creat one number
 		int num=-20;
 		
-		if(num>0)
+		if(num>0)//check the number is > 0 condition
 		{
 			System.out.println("Positive");
 		}else
