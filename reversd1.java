ba402b7d8323cd2f70643cdbb95f0e6afb234c52
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:32:38 +0530

    Add files via upload

---
+++ reversd1.java
@@ -0,0 +1,26 @@
+
+class HelloWorld {
+    
+    public static String str="muneera";
+    //convert str into arr;
+    public static char[] arr = str.toCharArray();
+    public static void main(String[] args)
+    {
+        System.out.println(arr);
+        ReverseSubString(0 , arr.length-1 , arr);//reverse the the string
+        System.out.println(arr);// print the revers string
+    }
+    public static void ReverseSubString(int start , int end ,char[] arr)
+    {
+        while (start <= end)
+        {
+            char num = arr [start];
+            arr [start] = arr [end];
+            arr [end] = num;
+            start ++;
+            end --;
+        }
+    }
+    
+}
+
