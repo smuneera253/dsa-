1be810c7bc085e524bc759eae6650db26666e111
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-05 19:47:17 +0530

    done
    done

--- evenoddd1.java
+++ evenoddd1.java
@@ -2,7 +2,10 @@ class evenodd
 {
 	public static void main(String[]args)
 	{
-	int num=7;
+		//creat one number the number 
+	        int num=7;
+		
+		//check the number is even or odd with condition
 		if(num%2==0)
 		{
 			System.out.println("Even");
